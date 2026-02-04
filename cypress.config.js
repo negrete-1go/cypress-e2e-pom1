f5662fb1c2a32f22ae4adb4109e63837ad346d1b
negrete-1go <negrete_1go@hotmail.com>
2026-02-04 12:59:05 -0600

    Add Cypress E2E framework structure and configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,32 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true
+  },
 
   e2e: {
+
+    baseUrl: "https://www.epam.com",
+    
+    video: false,
+    screenshotOnRunFailure: true,
+
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
+
+    testIsolation: true,
+  
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
     },
   },
 });
